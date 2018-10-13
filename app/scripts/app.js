e10e5f30b67f84fea638b45ae54504ef8fea618c
Zach Schneider <schneider61938@gmail.com>
2018-10-13 14:28:55 -0500

    added signup to index.html and app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,6 +28,11 @@ angular
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl',
         controllerAs: 'login'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl',
+        controllerAs: 'signup'
       });
     $locationProvider.hashPrefix('');
   });
