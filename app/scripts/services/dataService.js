3ab96ef358d9a27184cfabe5c3ad240da662ac11
Chris <carcher1204@gmail.com>
2018-10-14 03:51:00 -0500

    Adding support for current user, fixing lib issues

--- app/scripts/services/dataService.js
+++ app/scripts/services/dataService.js
@@ -7,7 +7,7 @@
  * # DataService
  * Controller of the globalHack7App
  */
-angular.module('globalHack7App').service('DataService', ['$window', '$location', '$http', function ($http, $window, $location) {
+angular.module('globalHack7App').service('DataService', ['$http', '$window', '$location', function ($http, $window, $location) {
   this.getData = function() {
     $http({
       method: 'GET',
@@ -30,30 +30,27 @@ angular.module('globalHack7App').service('DataService', ['$window', '$location',
   // This is the post request for registration
   this.registerUser = function(userInfo) {
     console.log(JSON.stringify(userInfo));
-    var config = 'headers: {' +
-      '"Content-Type": "text/plain"' +
-      '"Accept": "application/json"' +
-      '}';
-    $http.post('http://67.205.159.40/SignUp/', userInfo, config)
-    // $http({
-    //   method: 'POST',
-    //   url: 'http://67.205.159.40/SignUp/',
-    //   data: JSON.stringify(userInfo),
-    //   headers: {'Content-Type': 'application/json'}
-    //   // headers: {
-    //   //   "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNhcmNoZXIxMjA0QGdtYWlsLmNvbSIsIlVzZXJJZCI6IjIiLCJleHAiOjE1Mzk0NTQzMTIsImlzcyI6Ikdsb2JhbGhhY2siLCJhdWQiOiJHbG9iYWxoYWNrIn0.rVstjBZEX7_3B5AUOfP4GRCLaWzL_kOCnPnmJyZ9Ez8"
-    //   // }
-    // })
-      .then(function (res) {
+    $http({
+      method: 'POST',
+      url: 'http://67.205.159.40/SignUp/',
+      data: JSON.stringify(userInfo),
+      headers: {'Content-Type': 'application/json'}
+    }).then(function (res) {
         console.log(res);
-        if(res){
-          $window.localStorage.setItem("currentUser", angular.toJson(res));
-          return $location("/dashboard");
+        var currentUser = {};
+        if(!_.isUndefined(res)){
+          if(!_.isUndefined(res.data) && res.data.active) {
+            currentUser = res.data;
+            currentUser.authenticated = true;
+            $window.localStorage.setItem("currentUser", angular.toJson(currentUser));
+          } else {
+            $window.localStorage.setItem("currentUser", angular.toJson(currentUser));
+          }
+          return $location.url("/dashboard");
         }
-      })
-      .catch(function (e) {
+      }).catch(function (e) {
         console.log(e);
-        return $location("/signup");
+        return $location.url("/signup");
       });
   };
 }]);
